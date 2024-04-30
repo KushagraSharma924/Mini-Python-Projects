464c8dbd24d4c284efe637ee227d26fb11752377
Kushagra <143399362+KushagraSharma924@users.noreply.github.com>
2024-04-30 23:43:46 +0530

    Guessing Numbers..py

--- Guessing Numbers..py
+++ Guessing Numbers..py
@@ -1,9 +1,10 @@
+#a boring but a intresting one Game on python when you are not on your internet connection
 import random
-
+#Random module is a module to generate random results
 n = random.randrange(1,100)
 
 user_input = int(input("ENTER YOUR GUESS: "))
-
+# while loop used
 while n != user_input:
     if n < user_input:
         print("Too big number you guessed")
